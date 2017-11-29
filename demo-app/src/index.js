f98d6d3abc304927d2debb10ab731a1e14b9ab41
rafal-r <rafal@airr.pl>
2017-11-29 19:36:00 +0100

    airr-react main css file moved to node package

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,7 +1,7 @@
 import "babel-polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './airr-react.css';
+import '../node_modules/airr-react/css/airr-react.css';
 import './app.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
