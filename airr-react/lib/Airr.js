4c1fdd39f9efa56f1e91b207901412135d800b3a
rafal-r <rafal@airr.pl>
2017-12-06 10:42:59 +0100

    project structure reorganized

--- airr-react/lib/Airr.js
+++
@@ -1,9 +0,0 @@
-import Component from './AirrComponent';
-import View from './AirrView';
-import Scene from './AirrScene';
-import Mayer from './AirrMayer';
-import Sidepanel from './AirrSidepanel';
-import CompositeScene from './AirrCompositeScene';
-import FX from './AirrFX';
-
-export {Component, View, Scene, Mayer, Sidepanel, CompositeScene, FX};
