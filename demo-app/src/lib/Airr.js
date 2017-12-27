2fff2098c4cc148cbf5e51c201654bd2aa0cf402
rafal-r <rafal@airr.pl>
2017-12-27 15:31:06 +0100

    airr lib moved to demo-app

---
+++ demo-app/src/lib/Airr.js
@@ -0,0 +1,9 @@
+import Component from './AirrComponent';
+import View from './AirrView';
+import Scene from './AirrScene';
+import Mayer from './AirrMayer';
+import Sidepanel from './AirrSidepanel';
+import CompositeScene from './AirrCompositeScene';
+import FX from './AirrFX';
+
+export {Component, View, Scene, Mayer, Sidepanel, CompositeScene, FX};
