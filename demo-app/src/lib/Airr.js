86a2ee8858abff34092da28e150369cb687c6748
rafal-r <rajtar.rafal@gmail.com>
2018-03-14 00:10:48 +0100

    helpers exposed

--- demo-app/src/lib/Airr.js
+++ demo-app/src/lib/Airr.js
@@ -1,9 +1,11 @@
-import Component from './AirrComponent';
-import View from './AirrView';
-import Scene from './AirrScene';
-import Mayer from './AirrMayer';
-import Sidepanel from './AirrSidepanel';
-import CompositeScene from './AirrCompositeScene';
-import FX from './AirrFX';
+import Component from "./AirrComponent";
+import View from "./AirrView";
+import Scene from "./AirrScene";
+import Mayer from "./AirrMayer";
+import Sidepanel from "./AirrSidepanel";
+import CompositeScene from "./AirrCompositeScene";
+import FX from "./AirrFX";
+import { supportPassive, isMobileDevice } from "./eventHelpers";
 
-export {Component, View, Scene, Mayer, Sidepanel, CompositeScene, FX};
+export const Helpers = { supportPassive, isMobileDevice };
+export { Component, View, Scene, Mayer, Sidepanel, CompositeScene, FX };
--- dist/Airr.js
+++ dist/Airr.js
@@ -1,40 +1,43 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.FX = exports.CompositeScene = exports.Sidepanel = exports.Mayer = exports.Scene = exports.View = exports.Component = undefined;
+exports.FX = exports.CompositeScene = exports.Sidepanel = exports.Mayer = exports.Scene = exports.View = exports.Component = exports.Helpers = undefined;
 
-var _AirrComponent = require('./AirrComponent');
+var _AirrComponent = require("./AirrComponent");
 
 var _AirrComponent2 = _interopRequireDefault(_AirrComponent);
 
-var _AirrView = require('./AirrView');
+var _AirrView = require("./AirrView");
 
 var _AirrView2 = _interopRequireDefault(_AirrView);
 
-var _AirrScene = require('./AirrScene');
+var _AirrScene = require("./AirrScene");
 
 var _AirrScene2 = _interopRequireDefault(_AirrScene);
 
-var _AirrMayer = require('./AirrMayer');
+var _AirrMayer = require("./AirrMayer");
 
 var _AirrMayer2 = _interopRequireDefault(_AirrMayer);
 
-var _AirrSidepanel = require('./AirrSidepanel');
+var _AirrSidepanel = require("./AirrSidepanel");
 
 var _AirrSidepanel2 = _interopRequireDefault(_AirrSidepanel);
 
-var _AirrCompositeScene = require('./AirrCompositeScene');
+var _AirrCompositeScene = require("./AirrCompositeScene");
 
 var _AirrCompositeScene2 = _interopRequireDefault(_AirrCompositeScene);
 
-var _AirrFX = require('./AirrFX');
+var _AirrFX = require("./AirrFX");
 
 var _AirrFX2 = _interopRequireDefault(_AirrFX);
 
+var _eventHelpers = require("./eventHelpers");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var Helpers = exports.Helpers = { supportPassive: _eventHelpers.supportPassive, isMobileDevice: _eventHelpers.isMobileDevice };
 exports.Component = _AirrComponent2.default;
 exports.View = _AirrView2.default;
 exports.Scene = _AirrScene2.default;
