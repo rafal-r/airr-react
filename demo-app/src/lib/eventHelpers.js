e28612e2f2706ee19c88105649dbbc8e573fc6a8
rafal-r <rajtar.rafal@gmail.com>
2018-11-23 00:16:33 +0100

    fix

--- demo-app/src/lib/eventHelpers.js
+++ demo-app/src/lib/eventHelpers.js
@@ -1,12 +1,17 @@
 let supportPassive = false;
 // create options object with a getter to see if its passive property is accessed
-const opts = Object.defineProperty && Object.defineProperty({}, 'passive', {get: function () {
-    supportPassive = {passive:true};
-}});
+const opts =
+    Object.defineProperty &&
+    Object.defineProperty({}, "passive", {
+        get: function() {
+            supportPassive = { passive: true };
+            return supportPassive;
+        }
+    });
 // create a throwaway element & event and (synchronously) test out our options
-document.addEventListener('test', () => {}, opts);
+document.addEventListener("test", () => {}, opts);
 
 const mobileTest = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/;
 const isMobileDevice = mobileTest.test(navigator.userAgent);
 
-export {supportPassive, isMobileDevice};
+export { supportPassive, isMobileDevice };
--- dist/eventHelpers.js
+++ dist/eventHelpers.js
@@ -1,15 +1,18 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var supportPassive = false;
 
-var opts = Object.defineProperty && Object.defineProperty({}, 'passive', { get: function get() {
+var opts = Object.defineProperty && Object.defineProperty({}, "passive", {
+    get: function get() {
         exports.supportPassive = supportPassive = { passive: true };
-    } });
+        return supportPassive;
+    }
+});
 
-document.addEventListener('test', function () {}, opts);
+document.addEventListener("test", function () {}, opts);
 
 var mobileTest = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/;
 var isMobileDevice = mobileTest.test(navigator.userAgent);
