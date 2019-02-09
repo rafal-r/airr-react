4e7954acc50a6579768681c53cdb39f125ef8248
rafal-r <rajtar.rafal@gmail.com>
2019-02-09 17:00:11 +0100

    config files

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		jest: true
+	},
+	parser: "babel-eslint",
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"prettier",
+		"prettier/react"
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: "module"
+	},
+	plugins: ["react", "prettier"],
+	rules: {
+		"no-console": 0,
+		"no-unused-vars": ["error", { args: "none" }],
+		"prettier/prettier": [
+			"error",
+			{
+				printWidth: 100,
+				useTabs: true,
+				semi: true,
+				tabWidth: 4
+			}
+		]
+	},
+	settings: {
+		react: {
+			version: "16.7.0"
+		}
+	}
+};
